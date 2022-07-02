891dae1fd3a9204bbd6a565ff411a66ab7250242
Stuart Reid <stuart1334@gmail.com>
2022-07-02 17:14:58 +0100

    Back to boxes, but better

--- src/algrebra.rs
+++ src/algrebra.rs
@@ -1,34 +1,13 @@
-use std::fmt::{Display, LowerExp};
+use std::fmt::Display;
 
 /// A mathematical expression
 #[derive(Debug)]
 pub enum Expression {
-    Sum(Box<Expression>, Box<Expression>),
-    Product(Box<Expression>, Box<Expression>),
-    Group(Box<Expression>),
-    Term(Box<Term>),
-}
-
-impl Expression {
-  #[inline]
-  pub fn sum(left: Expression, right: Expression) -> Expression {
-    Self::Sum(Box::new(left), Box::new(right))
-  }
-
-  #[inline]
-  pub fn product(left: Expression, right: Expression) -> Expression {
-    Self::Product(Box::new(left), Box::new(right))
-  }
-
-  #[inline]
-  pub fn group(root: Expression) -> Expression {
-    Self::Group(Box::new(root))
-  }
-
-  #[inline]
-  pub fn term(term: Term) -> Expression {
-    Self::Term(Box::new(term))
-  }
+    Sum(Box<Self>, Box<Self>),
+    Product(Box<Self>, Box<Self>),
+    Group(Box<Self>),
+    Integer(i64),
+    Variable(i64, String),
 }
 
 impl Display for Expression {
@@ -37,35 +16,22 @@ impl Display for Expression {
             Expression::Sum(l, r) => write!(f, "{} + {}", l, r),
             Expression::Product(l, r) => write!(f, "{} * {}", l, r),
             Expression::Group(g) => write!(f, "({})", g),
-            Expression::Term(t) => write!(f, "{}", t),
-        }
-    }
-}
-
-/// A base level term in a mathematical expression
-#[derive(Debug)]
-pub enum Term {
-  /// A variable term, composed of an integer coefficient & a label
-    Variable(i64, String),
-
-    /// A constant integer term
-    Constant(i64),
-}
-
-impl Display for Term {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Term::Variable(c, v) => write!(f, "{}{}", c, v),
-            Term::Constant(v) => write!(f, "{}", v),
+            Expression::Integer(i) => write!(f, "{}", i),
+            Expression::Variable(c, v) => {
+                if *c == 1 {
+                    write!(f, "{}", v)
+                } else {
+                    write!(f, "{}{}", c, v)
+                }
+            }
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Expression, Term};
+    use super::{Expression};
 
     #[test]
-    fn test_sum_simplify() {
-    }
+    fn test_sum_simplify() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,13 @@ use crate::algrebra::*;
 mod algrebra;
 
 fn main() {
+    let expr = Expression::Sum(
+      Box::new(Expression::Group(Box::new(Expression::Sum(
+            Box::new(Expression::Variable(1, "a".to_string())),
+            Box::new(Expression::Variable(1, "b".to_string())),
+        )))),
+        Box::new(Expression::Variable(1, "c".to_string()))
+    );
+
+    println!("{}", expr);
 }
