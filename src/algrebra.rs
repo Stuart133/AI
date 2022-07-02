8be06aca2194b14e550e1f83c9d4bf31111f9c65
Stuart Reid <stuart1334@gmail.com>
2022-07-02 17:28:47 +0100

    Use vecs of terms for sums

--- src/algrebra.rs
+++ src/algrebra.rs
@@ -3,18 +3,35 @@ use std::fmt::Display;
 /// A mathematical expression
 #[derive(Debug)]
 pub enum Expression {
-    Sum(Box<Self>, Box<Self>),
-    Product(Box<Self>, Box<Self>),
+    Sum(Vec<Self>),
+    Product(Vec<Self>),
     Group(Box<Self>),
     Integer(i64),
     Variable(i64, String),
 }
 
+impl Expression {
+    pub fn group(root: Self) -> Self {
+      Expression::Group(Box::new(root))
+    }
+
+
+    pub fn simplify(&mut self) {
+        match self {
+            Expression::Sum(_) => todo!(),
+            Expression::Product(_) => todo!(),
+            Expression::Group(_) => todo!(),
+            Expression::Integer(_) => {}
+            Expression::Variable(_, _) => {}
+        }
+    }
+}
+
 impl Display for Expression {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Expression::Sum(l, r) => write!(f, "{} + {}", l, r),
-            Expression::Product(l, r) => write!(f, "{} * {}", l, r),
+            Expression::Sum(terms) => write_terms(terms, f, "+"),
+            Expression::Product(terms) => write_terms(terms, f, "*"),
             Expression::Group(g) => write!(f, "({})", g),
             Expression::Integer(i) => write!(f, "{}", i),
             Expression::Variable(c, v) => {
@@ -28,10 +45,30 @@ impl Display for Expression {
     }
 }
 
+fn write_terms<'a>(
+    terms: &Vec<Expression>,
+    f: &mut std::fmt::Formatter<'_>,
+    sep: &'a str,
+) -> std::fmt::Result {
+    for i in 0..terms.len() - 1 {
+        write!(f, "{} + ", terms[i]);
+    }
+
+    write!(f, "{}", terms[terms.len() - 1])
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{Expression};
+    use super::Expression;
 
     #[test]
-    fn test_sum_simplify() {}
+    fn test_sum_simplify() {
+        let expr = Expression::Sum(
+            Box::new(Expression::Group(Box::new(Expression::Sum(
+                Box::new(Expression::Variable(1, "a".to_string())),
+                Box::new(Expression::Variable(1, "b".to_string())),
+            )))),
+            Box::new(Expression::Variable(1, "c".to_string())),
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use crate::algrebra::*;
 mod algrebra;
 
 fn main() {
-    let expr = Expression::Sum(
-      Box::new(Expression::Group(Box::new(Expression::Sum(
-            Box::new(Expression::Variable(1, "a".to_string())),
-            Box::new(Expression::Variable(1, "b".to_string())),
-        )))),
-        Box::new(Expression::Variable(1, "c".to_string()))
-    );
+    let expr = Expression::Sum(vec![Expression::group(
+        Expression::Sum(vec![
+            Expression::Variable(1, "a".to_string()),
+            Expression::Variable(1, "b".to_string()),
+        ]),
+    ),
+    Expression::Variable(1, "c".to_string())]);
 
     println!("{}", expr);
 }
