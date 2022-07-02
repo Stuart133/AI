663efee5bd354ca0835396460871b955374e7d12
Stuart Reid <stuart1334@gmail.com>
2022-07-02 15:36:48 +0100

    Add basic expression structure

---
+++ src/algrebra.rs
@@ -0,0 +1,70 @@
+use std::fmt::Display;
+
+/// A mathematical expression
+#[derive(Debug)]
+pub enum Expression {
+    Sum(Box<Sum>),
+    Product(Box<Product>),
+    Term(String),
+}
+
+impl Display for Expression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Expression::Sum(s) => write!(f, "{}", s),
+            Expression::Product(p) => write!(f, "{}", p),
+            Expression::Term(t) => write!(f, "{}", t),
+        }
+    }
+}
+
+/// A sum of two expressions
+#[derive(Debug)]
+pub struct Sum {
+    left: Expression,
+    right: Expression,
+}
+
+impl Sum {
+    pub fn new(left: Expression, right: Expression) -> Self {
+        Self { left, right }
+    }
+}
+
+impl Display for Sum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} + {}", self.left, self.right)
+    }
+}
+
+/// A product of two expressions
+#[derive(Debug)]
+pub struct Product {
+    left: Expression,
+    right: Expression,
+}
+
+impl Display for Product {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} * {}", self.left, self.right)
+    }
+}
+
+impl Product {
+    pub fn new(left: Expression, right: Expression) -> Self {
+        Self { left, right }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Expression, Sum};
+
+    #[test]
+    fn test_sum_simplify() {
+        let expr = Sum::new(
+            Expression::Term("x".to_string()),
+            Expression::Term("y".to_string()),
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use crate::algrebra::*;
+
+mod algrebra;
+
 fn main() {
-    println!("Hello, world!");
+    let expr = Sum::new(
+        Expression::Term("x".to_string()),
+        Expression::Product(Box::new(Product::new(
+            Expression::Term("y".to_string()),
+            Expression::Term("z".to_string()),
+        ))),
+    );
+
+    println!("{}", expr);
 }
