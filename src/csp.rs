cff928c150ea40615a0e9278cd35074dfbae6b0e
Stuart Reid <stuart1334@gmail.com>
2022-07-23 18:05:41 +0100

    Continue CSP solver

--- src/csp.rs
+++ src/csp.rs
@@ -1,25 +1,40 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, hash::Hash};
 
-#[derive(Debug)]
-pub struct Variable<T> {
+#[derive(Debug, Clone)]
+pub struct Variable<T: Eq + Hash + Clone> {
     name: String,
     value: Option<T>,
     domain: HashSet<T>,
 }
 
-#[derive(Debug)]
+impl<T: Eq + Hash + Clone> Variable<T> {
+    pub fn new(name: String, domain: HashSet<T>, value: Option<T>) -> Self {
+        Variable {
+            name,
+            value,
+            domain,
+        }
+    }
+
+    pub fn assign(&mut self, value: &T) {
+      self.value = self.domain.take(value);
+      self.domain.clear();
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct BinaryConstraint<T> {
     check: fn(T, T) -> bool,
 }
 
 /// A generic constraint solver, with variables of type T
-#[derive(Debug)]
-pub struct ConstraintSolver<T> {
+#[derive(Debug, Clone)]
+pub struct ConstraintSolver<T: Eq + Hash + Clone> {
     variables: Vec<Variable<T>>,
     constraints: HashMap<(String, String), BinaryConstraint<T>>,
 }
 
-impl<T> ConstraintSolver<T> {
+impl<T: Eq + Hash + Clone> ConstraintSolver<T> {
     pub fn new(
         variables: Vec<Variable<T>>,
         constraints: HashMap<(String, String), BinaryConstraint<T>>,
@@ -28,5 +43,23 @@ impl<T> ConstraintSolver<T> {
             variables,
             constraints,
         }
+    }
+
+    pub fn solve(&self) -> Vec<Variable<T>> {
+        for (i, variable) in self.variables.iter().enumerate() {
+          for value in variable.domain.iter() {
+            let mut new_csp = self.clone();
+            new_csp.variables[i].assign(value);
+
+            // Check constraints
+
+            // Check if we're finished
+
+            // Continue DFS
+            return new_csp.solve();
+          }
+        }
+
+        vec![]
     }
 }
