2117bb47ce96ac4c9072887e68df98d24be5c6d5
Stuart Reid <stuart1334@gmail.com>
2022-07-28 13:34:33 +0100

    Add k neighbours classifier

--- src/senate.rs
+++ src/senate.rs
@@ -1,13 +1,16 @@
-use std::path::Path;
+use std::{
+    collections::{BinaryHeap, HashMap},
+    path::Path,
+};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct Legislator {
     name: String,
     party: Party,
     votes: Vec<Vote>,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum Party {
     Democrat,
     Republican,
@@ -24,7 +27,7 @@ impl Party {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Vote {
     Yea,
     Nay,
@@ -58,4 +61,78 @@ pub fn parse(data_file: &Path) -> Vec<Legislator> {
             votes: Vote::new_votes(&line[36..]),
         })
         .collect()
+}
+
+pub struct NearestNeighboursClassifier {
+    distance: fn(&Legislator, &Legislator) -> i64,
+    k: usize,
+    data: Vec<Legislator>,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct Distance<'a> {
+    distance: i64,
+    data: &'a Legislator,
+}
+
+impl<'a> Ord for Distance<'a> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        // Reverse the order as we are using this in a min heap
+        other.distance.cmp(&self.distance)
+    }
+}
+
+impl<'a> PartialOrd for Distance<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl NearestNeighboursClassifier {
+    pub fn new(
+        distance: fn(&Legislator, &Legislator) -> i64,
+        k: usize,
+        training_data: Vec<Legislator>,
+    ) -> Self {
+        NearestNeighboursClassifier {
+            distance,
+            k,
+            data: training_data,
+        }
+    }
+
+    pub fn classify(&self, query: &Legislator) -> Party {
+        let distances: Vec<Distance> = self
+            .data
+            .iter()
+            .map(|l| Distance {
+                distance: (self.distance)(l, query),
+                data: l,
+            })
+            .collect::<BinaryHeap<Distance>>()
+            .into_iter()
+            .take(self.k)
+            .collect();
+
+        let mut map = HashMap::new();
+        for i in distances.iter() {
+            match map.insert(i, 1_i64) {
+                Some(v) => {
+                    *map.get_mut(i).unwrap() += v;
+                }
+                None => {}
+            }
+        }
+
+        let mut best_count = 0;
+        let mut best_party = Party::Democrat;
+        for (k, v) in map {
+            if v > best_count {
+                best_count = 0;
+                best_party = k.data.party;
+            }
+        }
+
+        return best_party;
+    }
 }
